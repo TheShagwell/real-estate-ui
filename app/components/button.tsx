5d8e63f53e5c438ec179fbf594c95796cc8f129b
TheShagwell <kelvinerewharighe@gmail.com>
2023-12-28 08:04:50 +0100

    editable

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const button = () => {
   return (
-    <div className='px-6 py-4 flex justify-center'>button</div>
+    <div className='px-6 py-4 flex justify-center'>
+        buttons
+    </div>
   )
 }
 
