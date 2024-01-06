ad7d71a299f776c2f000d13fa2288ec21009dd1c
TheShagwell <kelvinerewharighe@gmail.com>
2024-01-06 22:14:28 +0100

    healthical

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div className='max-container'>
+    <div className='max-container py-5 px-20 mt-margin-48'>
       about
     </div>
   )
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div className='max-container py-5 px-20 mt-margin-48'>
-      header
+    <div className='max-container mb-20 py-5 px-20 mt-margin-48'>
+      header is here
     </div>
   )
 }
