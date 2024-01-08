9f98f41fae0b39785469e865d736ed877c2bf172
TheShagwell <kelvinerewharighe@gmail.com>
2024-01-08 09:27:39 +0100

    props added

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 type ButtonProps = {
@@ -9,11 +10,12 @@ type ButtonProps = {
   isReversed?: boolean;
 }
 
-const button = ({type, title} : ButtonProps) => {
+const button = ({type, title, icon, full, isReversed, variant} : ButtonProps) => {
   return (
     <button 
       className={`px-6 py-4 flex justify-center text-system-purple-60`} type={type}>
         <span className='cursor-pointer'>{title}</span>
+        {icon && <Image src={icon} alt={title} width={20} height={20}/>}
     </button>
   )
 }
