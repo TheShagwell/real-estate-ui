c6139ce29d443f7ad8f73c26f799aa19639e6b41
TheShagwell <kelvinerewharighe@gmail.com>
2024-01-20 05:30:13 +0100

    updated...

---
+++ app/constants/constant.index.ts
@@ -0,0 +1,18 @@
+// Index page 
+export const Inview = [
+    {
+        id: 'item-1',
+        header: '200+',
+        testament: 'Customers',
+    },
+    {
+        id: 'item-2',
+        header: '10k+',
+        testament: 'Properties For Clients',
+    },
+    {
+        id: 'item-3',
+        header: '16+',
+        testament: 'Years of Experience',
+    },
+]
