a53c3a91a29b900736df07ba64e739f60a4ce09b
Ezra Erewharighe <kelvinerewharighe@gmail.com>
2024-05-05 12:15:51 +0100

    Updated...

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import { Button, buttonVariants } from "@/components/ui/button"
 import Link from 'next/link'
+import Banner from '../components/banner'
 
 
 const About = () => {
   return (
     <div className='max-container px-5 py-12 mt-margin-48'>
+      <Banner/>
         About
         <Button className='mt-3' variant="destructive">Destructive</Button>
         <Link href="/" className={buttonVariants({ variant: "outline" })}>Click here</Link>
--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,17 +1,22 @@
+import { DoorClosed } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
-export default function banner({}: Props) {
+export default function Banner({}: Props) {
   return (
     <>
-      <div>
-        <span>✨Discover Your Dream Property with Estatein</span>
-        <Link href="/" className="underline">Learn More</Link>
-      </div>
-      <div className="absolute top-0 right-8">
-        
+      <div className="relative">
+        <div className="flex justify-center items-center gap-[.625rem]">
+          <span>✨Discover Your Dream Property with Estatein</span>
+          <Link href="/" className="underline">
+            Learn More
+          </Link>
+        </div>
+        <div className="flex items-center justify-center absolute top-0 right-8 border border-solid border-red-50 rounded-full w-8 h-8">
+            <DoorClosed/>
+        </div>
       </div>
     </>
   );
