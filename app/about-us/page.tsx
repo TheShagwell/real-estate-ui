1614b599638794633dda4aeafee24a2b81e5c82d
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-05-07 04:08:41 +0100

    Updated...

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,18 +2,14 @@
 import React from 'react'
 import { Button, buttonVariants } from "@/components/ui/button"
 import Link from 'next/link'
-import Banner from '../components/banner'
-import Header from '../components/header'
-import Hero from '../components/Hero'
+
 
 
 const About = () => {
   return (
-    <div className=''>
-      <Banner/>
-      <Header/>
-      <Hero/>
-    </div>
+    <>
+      <p>Hello, We are coming Live to you.</p>
+    </>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 
 export default function Home() {
   return (
     <>
-      <p>Am Home</p>
+      <div className=''>
+      <Banner/>
+      <Header/>
+      <Hero/>
+    </div>
     </>
   );
 }
