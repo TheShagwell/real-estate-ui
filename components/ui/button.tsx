812faaf9b72d6aba36027a940775c21e6e832374
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-05-13 11:30:12 +0100

    Updated...

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,8 +14,8 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        estateinbg: "bg-[#703BF7] text-white",
-        estateinbgOutline: "text-white  border border-solid border-[#262626]",
+        estateinbg: "bg-[#703BF7] font-medium text-white",
+        estateinbgOutline: "text-white font-medium border border-solid border-[#262626]",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
