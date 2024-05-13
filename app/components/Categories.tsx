146aca06ddeb97030402a89c41439cb202759750
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-05-13 11:54:50 +0100

    Updated...

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -5,7 +5,11 @@ type Props = {}
 export default function Categories({}: Props) {
   return (
     <>
-      Categories
+      <div className="flex flex-col p-4 relative">
+        <div className="absolute right-0 top-0"></div>
+        <div className="border border-separate mt-[1.875rem]"></div>
+        <p></p>
+      </div>
     </>
   )
 }
