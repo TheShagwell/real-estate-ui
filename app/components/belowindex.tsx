6f57827825a9863febcb12d8222455c3e0c6a480
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-06-16 02:51:45 +0100

    Updated...

--- app/components/belowindex.tsx
+++ app/components/belowindex.tsx
@@ -9,9 +9,9 @@ export default function belowindex({}: Props) {
             <div className="flex flex-row justify-between items-center">
                 <div className="m-w-[61.1875em]">
                 <h3>Start Your Real Estate Journey Today</h3>
+                <p>Your dream property is just a click away. Whether you're looking for a new home, a strategic investment, or expert real estate advice, Estatein is here to assist you every step of the way. Take the first step towards your real estate goals and explore our available properties or get in touch with our team for personalized assistance.</p>
 
                 </div>
-                <p>Your dream property is just a click away. Whether you're looking for a new home, a strategic investment, or expert real estate advice, Estatein is here to assist you every step of the way. Take the first step towards your real estate goals and explore our available properties or get in touch with our team for personalized assistance.</p>
             </div>
         </div>
     </>
