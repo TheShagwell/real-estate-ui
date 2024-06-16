c605e829c3fa9482bac138269b7ec0e02e81dd38
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-06-16 02:52:11 +0100

    Updated...

--- app/components/belowindex.tsx
+++ app/components/belowindex.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function belowindex({}: Props) {
   return (
     <>
-        <div className=''>
-            <div className="flex flex-row justify-between items-center">
-                <div className="m-w-[61.1875em]">
-                <h3>Start Your Real Estate Journey Today</h3>
-                <p>Your dream property is just a click away. Whether you're looking for a new home, a strategic investment, or expert real estate advice, Estatein is here to assist you every step of the way. Take the first step towards your real estate goals and explore our available properties or get in touch with our team for personalized assistance.</p>
-
-                </div>
-            </div>
+      <div className="">
+        <div className="flex flex-row justify-between items-center">
+          <div className="m-w-[61.1875em]">
+            <h3>Start Your Real Estate Journey Today</h3>
+            <p>
+              Your dream property is just a click away. Whether you're looking
+              for a new home, a strategic investment, or expert real estate
+              advice, Estatein is here to assist you every step of the way. Take
+              the first step towards your real estate goals and explore our
+              available properties or get in touch with our team for
+              personalized assistance.
+            </p>
+          </div>
         </div>
+      </div>
     </>
-
-  )
+  );
 }
